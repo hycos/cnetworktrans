5f4a86498a513066e04756f3070e51e5c26933de
Julian Thome <julian.thome@uni.lu>
2017-10-09 23:31:28 +0200

    header

--- src/main/java/org/snt/cnetworktrans/lang/cvc4/CVC4Translator.java
+++ src/main/java/org/snt/cnetworktrans/lang/cvc4/CVC4Translator.java
@@ -1,3 +1,20 @@
+/*
+ * cnetworktrans - translate constraint network to various output formats
+ * Copyright (C) 2017 Julian Thome <julian.thome.de@gmail.com>
+ *
+ * cnetworktrans is licensed under the EUPL, Version 1.1 or – as soon
+ * they will be approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence"); You may not use this work except in compliance with the
+ * Licence. You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/sites/default/files/eupl1.1.-licence-en_0.pdf
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the Licence for the
+ * specific language governing permissions and limitations under the Licence.
+ */
+
 package org.snt.cnetworktrans.lang.cvc4;
 
 import com.github.hycos.cnetwork.core.domain.range.BooleanRange;
